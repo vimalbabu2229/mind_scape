21f52c7b83ccede347af0f344971efb7797a0f58
Vimal Babu <vimalbabu2229@gmail.com>
2024-07-09 02:41:32 +0530

    Updated api docs

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,17 @@ from accounts.views import AccountsViewSet
 router: DefaultRouter = DefaultRouter()
 # ____________________ ACCOUNTS APIs ________________________
 router.register(r"accounts", AccountsViewSet, basename="accounts")
+"""
+End Points:
+
+    POST    /api/accounts/register/     ==> register user with username, email, password
+    POST    /api/accounts/login/        ==> login user with username and password
+    POST    /api/accounts/logout/       ==> logout authenticated users
+    DELETE  /api/accounts/delete/       ==> delete authenticated user account
+    GET     /api/accounts/profile/      ==> get complete profile details
+    PATCH   /api/accounts/profile/      ==> update profile details. Username and email cannot 
+                                            be changed 
+"""
 
 # Url patterns 
 urlpatterns = [
